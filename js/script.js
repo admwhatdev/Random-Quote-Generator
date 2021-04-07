37fbda64052e1ef0f72839fbc811e36ea5c76fe4
admwhatdev <81971695+admwhatdev@users.noreply.github.com>
2021-04-07 21:12:19 +0100

    First Commit
    Have created the skeleton of the random quotes generator. I need to clean up the code and do some research as there are definitely areas that can be improved.

--- js/script.js
+++ js/script.js
@@ -10,20 +10,85 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+  {
+    quote:"Before you marry a person, you should first make them use a computer with slow Internet to see who they really are.",
+    source: "Will Ferrel"
+  },
+  {
+    quote:"I used to sell furniture for a living. The trouble was, it was my own.",
+    source: "Les Dawson"
+  },
+  {
+    quote:"I never feel more alone than when I'm trying to put sunscreen on my back.",
+    source: "Jimmy Kimmel"
+  },
+  {
+    quote:"I'm not good at the advice. Can I interest you in a sarcastic comment?",
+    source: "Chandler (Matthew Perry)",
+    citation: "Friends",
+    year: 2002
+  },
+  {
+    quote:"I never forget a face - but in your case, I'll be glad to make an exception",
+    source: "Groucho Marx",
+    year: 1941
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
+let quoteHtml = '';
+function getRandomQuote() {
+  let quoteNumber = Math.floor(Math.random() * quotes.length );
+return quoteNumber;
+  };
 
-
+ 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
 
+  let quoteInfo = quotes[getRandomQuote()];
+  
+let quoteHtml = '';
 
+  if (!quoteInfo.citation  && !quoteInfo.year) {
+    quoteHtml += `
+      <p class="quote">${quoteInfo.quote}</p>
+      <p class ="source">${quoteInfo.source}</p>
+    `;
+  } else if (quoteInfo.year && !quoteInfo.citation ){
+    quoteHtml += `
+    <p class="quote">${quoteInfo.quote}</p>
+    <p class="source">${quoteInfo.source} 
+      <span class="year">${quoteInfo.year}</span>
+    </p>
+    `
+  } else if (quoteInfo.citation && !quoteInfo.year){
+    quoteHtml += `
+    <p class="quote">${quoteInfo.quote}</p>
+    <p class="source">${quoteInfo.source} 
+      <span class="citation">${quoteInfo.citation}</span>
+    </p>
+    `
+  } else {
+    quoteHtml += `
+    <p class="quote">${quoteInfo.quote}</p>
+    <p class="source">${quoteInfo.source} 
+      <span class="citation">${quoteInfo.citation}</span>
+      <span class="year">${quoteInfo.year}</span>
+    </p>
+    `
+  }
+  return document.getElementById('quote-box').innerHTML = `${quoteHtml}`; 
+ }
+  ;
+
+  printQuote();
 
 /***
  * click event listener for the print quote button
